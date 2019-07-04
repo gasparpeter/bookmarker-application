281fbf6e34be0b5f458b7bd722ec3fabdc788b86
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-04 15:06:14 +0300

    - function fetchBookmarks

--- bookmarker.js
+++ bookmarker.js
@@ -31,3 +31,24 @@ function saveBookmark(e) {
 
     e.preventDefault();
 }
+
+function fetchBookmarks(){
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    // Build output
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">'+
+            '<h3>'+name+
+            ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+            ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+            '</h3>'+
+            '</div>';
+    }
+}
