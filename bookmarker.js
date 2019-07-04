a5bdeb3b1c9ecd1a4e2887be70a2632f1e20d22b
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-04 14:30:23 +0300

    - javascript function, savebookmark

--- bookmarker.js
+++ bookmarker.js
@@ -1,1 +1,33 @@
-console.log("Asdsa")
+
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    };
+
+    
+    
+    if (localStorage.getItem('bookmarks') === null) {
+
+        var bookmarks = [];
+        bookmark.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }else {
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+        bookmarks.push(bookmark);
+
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+
+
+
+    e.preventDefault();
+}
